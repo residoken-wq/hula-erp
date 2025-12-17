7ef9fb810a756a33f796273893fe88330dbe1150
nhan nguyen <resido.ken@gmail.com>
2025-12-17 12:56:20 +0700

    fix user

---
+++ src/users/users.controller.ts
@@ -0,0 +1,56 @@
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly service: UsersService) {}
+
+  // --- USER APIs ---
+  @Get() 
+  getAll() { 
+      return this.service.getAllUsers(); 
+  }
+
+  @Post() 
+  create(@Body() body: any) { 
+      return this.service.createUser(body); 
+  }
+
+  @Put(':id') 
+  update(@Param('id') id: number, @Body() body: any) { 
+      return this.service.updateUser(id, body); 
+  }
+
+  @Delete(':id') 
+  delete(@Param('id') id: number) { 
+      return this.service.deleteUser(id); 
+  }
+
+  // --- GROUP APIs ---
+  @Get('groups') 
+  getGroups() { 
+      return this.service.getAllGroups(); 
+  }
+
+  @Get('groups/:id') 
+  getGroupDetail(@Param('id') id: number) { 
+      return this.service.getGroupDetail(id); 
+  }
+
+  @Post('groups') 
+  createGroup(@Body() body: any) { 
+      return this.service.createGroup(body); 
+  }
+  
+  // Update Group + Permission (Gọi service updateGroupPermissions)
+  @Post('groups/:id/permissions') // Sửa lại route cho khớp với Frontend gọi
+  updatePerms(@Param('id') id: number, @Body() body: any) { 
+      return this.service.updateGroupPermissions(id, body); 
+  }
+  
+  // API dự phòng nếu frontend gọi route cũ
+  @Post('groups/:id/update') 
+  updateGroup(@Param('id') id: number, @Body() body: any) { 
+      return this.service.updateGroupPermissions(id, body); 
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserGroup } from './entities/user-group.entity';
 import { GroupPermission } from './entities/group-permission.entity';
-import { UsersController } from './users.controller';
+import { UsersController } from './users.controller'; // <--- Đảm bảo file này tồn tại cùng thư mục
 import { UsersService } from './users.service';
 
 @Module({
