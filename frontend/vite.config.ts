cde7343d2b7fbc95f168f7e6526c295e9b6d6424
nhan nguyen <resido.ken@gmail.com>
2025-12-25 11:56:49 +0700

    feat: Add initial Vite configuration for React project.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // Cho phep Docker map port
-    port: 5173
+    port: 5173,
+    allowedHosts: ['erp.nemmamnon.com']
   }
 })
