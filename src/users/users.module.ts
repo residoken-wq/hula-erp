a2231e68339ae7d17f712638bb7cc55d7e5f97a2
nhan nguyen <resido.ken@gmail.com>
2025-12-26 11:01:04 +0700

    feat: create UsersModule for user management with TypeORM entities, controllers, services, and AuthModule integration.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,10 +6,15 @@ import { GroupPermission } from './entities/group-permission.entity';
 import { UsersController } from './users.controller'; // <--- Đảm bảo file này tồn tại cùng thư mục
 import { UsersService } from './users.service';
 
+import { AuthModule } from '../auth/auth.module'; // <--- IMPORT
+
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserGroup, GroupPermission])],
+  imports: [
+    TypeOrmModule.forFeature([User, UserGroup, GroupPermission]),
+    AuthModule // <--- Fix: Import AuthModule để dùng Guard
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
