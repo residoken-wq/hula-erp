be0240be41c3f2075cd6b21167a981357ced73fd
nhan nguyen <resido.ken@gmail.com>
2025-12-26 11:04:04 +0700

    feat: create AuthModule for JWT authentication with user integration and guard exports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
@@ -8,7 +8,7 @@ import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    UsersModule,
+    forwardRef(() => UsersModule),
     PassportModule,
     JwtModule.register({
       secret: 'HULA_SECRET_KEY',
