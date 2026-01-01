f3e71b3ffa56e32f6d3dc733d6a799e2320670fb
residoken-wq <resido.ken@gmail.com>
2026-01-01 23:40:54 +0700

    feat: Add initial UploadModule with TypeORM entities, controllers, and services.

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -4,22 +4,24 @@ import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { MaterialsModule } from '../materials/materials.module';
 import { ProductsModule } from '../products/products.module';
+import { SalesModule } from '../sales/sales.module';
 // Entities
 import { Product } from '../products/product.entity';
 import { BOM } from '../bom/bom.entity';
 import { ProductComponent } from '../products/product-component.entity';
-import { Customer } from '../customers/customer.entity'; // --- FIX: IMPORT CUSTOMER
+import { Customer } from '../customers/customer.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-        Product, BOM, ProductComponent,
-        Customer // --- FIX: KHAI BAO ENTITY CUSTOMER
-    ]), 
-    MaterialsModule, 
-    ProductsModule
-  ], 
+      Product, BOM, ProductComponent,
+      Customer
+    ]),
+    MaterialsModule,
+    ProductsModule,
+    SalesModule
+  ],
   controllers: [UploadController],
   providers: [UploadService],
 })
-export class UploadModule {}
+export class UploadModule { }
