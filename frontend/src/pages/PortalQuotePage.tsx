878f5eada04078f5d416724d647b631302e1e9e8
residoken-wq <resido.ken@gmail.com>
2026-01-03 15:20:17 +0700

    refactor: Remove redundant color style property

--- frontend/src/pages/PortalQuotePage.tsx
+++ frontend/src/pages/PortalQuotePage.tsx
@@ -635,8 +635,6 @@ const PortalQuotePage: React.FC = () => {
                                                 boxShadow: '0 1px 2px rgba(0,0,0,0.05)',
                                                 color: '#333'
                                             }}>
-                                                color: '#333'
-                                            }}>
                                                 <div dangerouslySetInnerHTML={{ __html: item.content }} />
                                             </div>
                                         </div>
