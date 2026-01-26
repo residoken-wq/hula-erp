e795ec4c5ecbe34b5117af17863b6bfb7ef66519
residoken-wq <resido.ken@gmail.com>
2026-01-26 13:19:28 +0700

    fix error

--- src/public/public.module.ts
+++ src/public/public.module.ts
@@ -5,6 +5,8 @@ import { Product } from '../products/product.entity';
 import { Category } from '../categories/category.entity';
 import { Customer } from '../customers/customer.entity';
 import { BlogPost } from '../blogs/blog-post.entity';
+import { SystemConfig } from '../system/system-config.entity';
+import { SalesModule } from '../sales/sales.module';
 import { ProductWebsiteConfig } from '../products/entities/product-website-config.entity';
 
 @Module({
