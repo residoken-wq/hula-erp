48f20cf39fe8c9a0e1b57d1561176b3ede6b5898
residoken-wq <resido.ken@gmail.com>
2026-05-04 23:21:53 +0700

    fix error

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -7,9 +7,13 @@ import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
 
 import { Transaction } from '../finance/transaction.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer, CustomerContact, CustomerComment, Transaction])],
+  imports: [
+    TypeOrmModule.forFeature([Customer, CustomerContact, CustomerComment, Transaction]),
+    AuthModule
+  ],
   controllers: [CustomersController],
   providers: [CustomersService],
   exports: [CustomersService],
