57284bf0e9dc1fed8695881da0e9d2bc0133a541
residoken-wq <resido.ken@gmail.com>
2026-06-04 23:12:19 +0700

    fix eror 500

--- src/purchasing/purchasing.service.ts
+++ src/purchasing/purchasing.service.ts
@@ -57,7 +57,7 @@ export class PurchasingService {
     }
 
     async getAllPOs() {
-        return this.poRepo.find({ relations: ['supplier', 'items', 'items.material', 'plan', 'plan.customer'], order: { created_at: 'DESC' } });
+        return this.poRepo.find({ relations: ['supplier', 'items', 'items.material', 'plan', 'plan.sales_orders', 'plan.sales_orders.customer'], order: { created_at: 'DESC' } });
     }
 
     async getPOByCode(code: string) {
