92273bb4e61048c434cf5da9a91f59eca9a92142
residoken-wq <resido.ken@gmail.com>
2026-06-12 20:17:26 +0700

    fix error

--- src/designs/designs.module.ts
+++ src/designs/designs.module.ts
@@ -5,10 +5,12 @@ import { DesignsService } from './designs.service';
 import { CustomerLogo } from './entities/customer-logo.entity';
 import { PrintDesign } from './entities/print-design.entity';
 import { PrintSample } from './entities/print-sample.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CustomerLogo, PrintDesign, PrintSample])
+    TypeOrmModule.forFeature([CustomerLogo, PrintDesign, PrintSample]),
+    AuthModule
   ],
   controllers: [DesignsController],
   providers: [DesignsService],
