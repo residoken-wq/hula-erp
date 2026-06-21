fcc42e82059ca0ca7d024eae350b95d1bdb0f02d
residoken-wq <resido.ken@gmail.com>
2026-06-21 15:26:21 +0700

    fix error

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -31,7 +31,7 @@ export class ProductsService {
 
     async findAll() {
         const products = await this.productRepo.find({
-            select: ['id', 'sku', 'name', 'category_id', 'product_type', 'cost_price', 'base_price', 'quantity_in_stock', 'booking_stock', 'approved_booking_stock', 'profit_margin', 'is_active', 'unit', 'customer_description', 'processing_description', 'vat_description', 'image_url', 'show_on_website', 'contact_for_price', 'website_price', 'website_sale_price', 'website_order', 'website_display_name', 'tags'],
+            select: ['id', 'sku', 'name', 'category_id', 'product_type', 'cost_price', 'base_price', 'quantity_in_stock', 'booking_stock', 'approved_booking_stock', 'profit_margin', 'is_active', 'is_flagged', 'unit', 'customer_description', 'processing_description', 'vat_description', 'image_url', 'show_on_website', 'contact_for_price', 'website_price', 'website_sale_price', 'website_order', 'website_display_name', 'tags'],
             order: { id: 'DESC' },
             relations: ['category_link']
         });
