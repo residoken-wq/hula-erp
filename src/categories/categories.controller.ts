ac9f4b4fba45e436a72e4244cd37d508481f551b
residoken-wq <resido.ken@gmail.com>
2026-08-27 07:46:55 +0700

    feat: implement CategoriesController with CRUD endpoints for category management

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -6,6 +6,7 @@ export class CategoriesController {
   constructor(private readonly s: CategoriesService) {}
 
   @Get() findAll() { return this.s.findAll(); }
+  @Get(':id') findOne(@Param('id') id: number) { return this.s.findOne(id); }
   @Post() create(@Body() b: any) { return this.s.create(b); }
   @Put(':id') update(@Param('id') id: number, @Body() b: any) { return this.s.update(id, b); }
   @Delete(':id') remove(@Param('id') id: number) { return this.s.remove(id); }
